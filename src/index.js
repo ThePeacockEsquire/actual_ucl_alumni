556823decce6b4f846c51adbe1c6bb5926998db9
Cameron Pavao <cam9548@gmail.com>
2018-04-12 00:50:17 -0400

    first real creation
    created main.js and main.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Main from './main';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('app'));
 registerServiceWorker();
