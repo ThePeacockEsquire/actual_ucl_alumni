b1cecc6d7ac781db7e8fd59618befdc22d5b6e44
Cameron Pavao <cam9548@gmail.com>
2018-04-25 12:37:37 -0400

    deleted a pic

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-//import 'startbootstrap/css/one-page-wonder.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
